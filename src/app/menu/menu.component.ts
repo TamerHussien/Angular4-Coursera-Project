768016400d5ba4da5f2b1f84a58985c942e1502d
Tamer Hussein <eng.tamerhussien@gmail.com>
2018-04-01 16:24:26 +0200

    Data Binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,40 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-
-const DISHES: Dish[] = [
-  {
-    name: 'Uthappizza',
-    image: '/assets/images/uthappizza.png',
-    category: 'mains',
-    label: 'Hot',
-    price: '4.99',
-    // tslint:disable-next-line:max-line-length
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
- {
-    name: 'Zucchipakoda',
-    image: '/assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    label: '',
-    price: '1.99',
-    // tslint:disable-next-line:max-line-length
-    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
- {
-    name: 'Vadonut',
-    image: '/assets/images/vadonut.png',
-    category: 'appetizer',
-    label: 'New',
-    price: '1.99',
-    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
- {
-    name: 'ElaiCheese Cake',
-    image: '/assets/images/elaicheesecake.png',
-    category: 'dessert',
-    label: '',
-    price: '2.99',
-    // tslint:disable-next-line:max-line-length
-    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
- ];
-
+import { DISHES } from '../shared/dishes';
 
 @Component({
   selector: 'app-menu',
@@ -45,10 +11,13 @@ export class MenuComponent implements OnInit {
 
   dishes = DISHES;
 
-  selectedDish: Dish = DISHES[0];
+  selectedDish: Dish;
   constructor() { }
 
   ngOnInit() {
   }
 
+onSelect(dish: Dish) {
+  this.selectedDish = dish;
+}
 }
