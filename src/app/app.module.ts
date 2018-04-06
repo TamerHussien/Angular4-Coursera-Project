e94c794b02937a3d278dec5b73f73eb98d61d212
Tamer Hussein <eng.tamerhussien@gmail.com>
2018-04-06 22:41:35 +0200

    angular services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 
 @NgModule({
@@ -31,7 +32,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule, MatIconModule, MatProgressSpinnerModule, MatDialogModule, FlexLayoutModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
