d80bc6edf725d928ae9ab3abeefc5c6626e3475f
Tamer Hussein <eng.tamerhussien@gmail>
2018-04-09 03:57:15 +0200

    Using promise with latency 2 sec

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -9,18 +9,24 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    return new Promise (resolve => {
+      setTimeout(() => resolve(LEADERS), 2000);
+    });
   }
 
   getLeader(id: number): Promise<Leader> {
 
-    return Promise.resolve(LEADERS.filter((dish) => dish.id === id)[0]);
+    return new Promise (resolve => {
+      setTimeout(() => resolve(LEADERS.filter((dish) => dish.id === id)[0]), 2000);
+    });
 
   }
 
   getFeaturedLeader(): Promise<Leader> {
 
-    return Promise.resolve(LEADERS.filter((dish) => dish.featured)[0]);
+    return new Promise (resolve => {
+      setTimeout(() => resolve(LEADERS.filter((dish) => dish.featured)[0]), 2000);
+    });
 
   }
 
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,24 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import { resolve } from 'url';
 
 @Injectable()
 export class PromotionService {
 
   constructor() { }
   getPromotions(): Promise<Promotion[]> {
-    return Promise.resolve(PROMOTIONS);
+    return new Promise(resolve => {
+        setTimeout(() => resolve(PROMOTIONS), 2000);
+    });
   }
 
   getPromotion(id: number): Promise<Promotion> {
 
-    return Promise.resolve(PROMOTIONS.filter((dish) => dish.id === id)[0]);
+    return new Promise (resolve => {
+      setTimeout(() => resolve(PROMOTIONS.filter((dish) => dish.id === id)[0])
+                        , 2000);
+    });
 
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
 
-    return Promise.resolve(PROMOTIONS.filter((dish) => dish.featured)[0]);
+    return new Promise (resolve => {
+      setTimeout(() => resolve(PROMOTIONS.filter((dish) => dish.featured)[0]), 2000);
+    });
 
   }
 }
